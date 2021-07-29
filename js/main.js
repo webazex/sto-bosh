06680ec0e53d152b192dd1c7500fd27b66e7b95f
webacoders <webacoders@gmail.com>
2021-07-29 14:25:27 +0300

    add sections to home page

--- js/main.js
+++ js/main.js
@@ -114,4 +114,38 @@ $(document).ready(function () {
             div.hide(300); // скрываем его
         }
     });
+    $('.slider-section__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
